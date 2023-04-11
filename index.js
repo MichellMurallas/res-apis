432b818636a0e1f59d25b064e7c0eecc52ddc380
MichellMurallas <michellsms893@gmail.com>
2023-04-10 20:00:33 -0500

    primer routing

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const rautes = require("./routes/index.js")
 
 //Crear el servidor:
 const app =  express();
+
+//Rutas de la app:
+app.use("/", rautes())
 
 //puerto:
 app.listen(5000)
